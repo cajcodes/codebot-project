c2050574216403e504baf78615b32f6e0844cc03
cajcodes <129537800+cajcodes@users.noreply.github.com>
2023-09-18 16:10:08 -0500

    Update img.js

--- public/img.js
+++ public/img.js
@@ -182,7 +182,7 @@ function getFetchUrl() {
   if (currentEngine === "gpt-4") {
     return 'https://us-central1-codebot-project.cloudfunctions.net/chatBotGpt4';
   } else if (currentEngine === "gpt-3.5") {
-    return 'https://us-central1-codebot-project.cloudfunctions.net/chatBotGpt35TurboInstruct';
+    return 'https://us-central1-codebot-project.cloudfunctions.net/chatBotGpt35Turbo';
   } else if (currentEngine === "palm") {
     // Add the URL for your PaLM engine
     return 'https://us-central1-codebot-project.cloudfunctions.net/chatBotGpt35TurboLarge';
